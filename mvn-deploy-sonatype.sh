dfee1de922f19bdb146484595cf06458de3df050
Dmitry Kotlyarov <kotlyarov.dmitry@gmail.com>
2020-06-17 00:44:12 +0300

    skip tests

--- mvn-deploy-sonatype.sh
+++ mvn-deploy-sonatype.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mvn clean
-mvn deploy -DaltDeploymentRepository=sonatype::default::https://oss.sonatype.org/service/local/staging/deploy/maven2
+mvn deploy -DaltDeploymentRepository=sonatype::default::https://oss.sonatype.org/service/local/staging/deploy/maven2 -DskipTests
