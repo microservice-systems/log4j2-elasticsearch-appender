aab7976e4201f2aa74a9bf5308810d90e7dc76d5
Dmitry Kotlyarov <kotlyarov.dmitry@gmail.com>
2021-04-21 01:27:59 +0300

    version 2.0.0

--- src/main/java/systems/microservice/log4j2/elasticsearch/appender/ElasticSearchAppender.java
+++ src/main/java/systems/microservice/log4j2/elasticsearch/appender/ElasticSearchAppender.java
@@ -44,6 +44,7 @@ import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Dmitry Kotlyarov
@@ -51,6 +52,8 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 @Plugin(name = "ElasticSearch", category = "Core", elementType = "appender", printObject = true)
 public final class ElasticSearchAppender extends AbstractAppender {
+    private static final AtomicReference<ElasticSearchAppender> INSTANCE = new AtomicReference<>(null);
+
     public static final long PROCESS_ID = createProcessID();
     public static final UUID PROCESS_UUID = createProcessUUID();
     public static final long PROCESS_START = createProcessStart();
@@ -457,23 +460,27 @@ public final class ElasticSearchAppender extends AbstractAppender {
 
     @Override
     public void start() {
-        super.start();
-        if (flushThread != null) {
-            enabled.set(true);
-            flushThread.setDaemon(false);
-            flushThread.start();
+        if (!enabled.get()) {
+            super.start();
+            if (flushThread != null) {
+                enabled.set(true);
+                flushThread.setDaemon(false);
+                flushThread.start();
+            }
         }
     }
 
     @Override
     public void stop() {
-        super.stop();
-        if (flushThread != null) {
-            enabled.set(false);
-            if (flushThread.isAlive()) {
-                try {
-                    flushThread.join();
-                } catch (InterruptedException e) {
+        if (enabled.get()) {
+            super.stop();
+            if (flushThread != null) {
+                enabled.set(false);
+                if (flushThread.isAlive()) {
+                    try {
+                        flushThread.join();
+                    } catch (InterruptedException e) {
+                    }
                 }
             }
         }
@@ -482,12 +489,6 @@ public final class ElasticSearchAppender extends AbstractAppender {
     @Override
     public void append(LogEvent event) {
         if (enabled.get()) {
-            String l = event.getLoggerName();
-            if (l != null) {
-                if (l.startsWith("org.apache.http.")) {
-                    return;
-                }
-            }
             String t = event.getThreadName();
             if (t != null) {
                 if (t.startsWith("log4j2-elasticsearch-appender-flush-")) {
@@ -539,28 +540,38 @@ public final class ElasticSearchAppender extends AbstractAppender {
                                                        @PluginAttribute("setDefaultUncaughtExceptionHandler") String setDefaultUncaughtExceptionHandler,
                                                        @PluginElement("Filter") Filter filter,
                                                        @PluginElement("Layout") Layout<? extends Serializable> layout) {
-        return new ElasticSearchAppender((name != null) ? name : "elasticsearch",
-                                         getProperty("log4j2.elasticsearch.url", "LOG4J2_ELASTICSEARCH_URL", url, null),
-                                         getProperty("log4j2.elasticsearch.user", "LOG4J2_ELASTICSEARCH_USER", user, null),
-                                         getProperty("log4j2.elasticsearch.password", "LOG4J2_ELASTICSEARCH_PASSWORD", password, null),
-                                         getProperty("log4j2.elasticsearch.index", "LOG4J2_ELASTICSEARCH_INDEX", index, null),
-                                         Boolean.parseBoolean(getProperty("log4j2.elasticsearch.enable", "LOG4J2_ELASTICSEARCH_ENABLE", enable, "true")),
-                                         Integer.parseInt(getProperty("log4j2.elasticsearch.count.max", "LOG4J2_ELASTICSEARCH_COUNT_MAX", countMax, "20000")),
-                                         Long.parseLong(getProperty("log4j2.elasticsearch.size.max", "LOG4J2_ELASTICSEARCH_SIZE_MAX", sizeMax, "10485760")),
-                                         Integer.parseInt(getProperty("log4j2.elasticsearch.bulk.count.max", "LOG4J2_ELASTICSEARCH_BULK_COUNT_MAX", bulkCountMax, "8000")),
-                                         Long.parseLong(getProperty("log4j2.elasticsearch.bulk.size.max", "LOG4J2_ELASTICSEARCH_BULK_SIZE_MAX", bulkSizeMax, "4194304")),
-                                         Long.parseLong(getProperty("log4j2.elasticsearch.delay.max", "LOG4J2_ELASTICSEARCH_DELAY_MAX", delayMax, "60")) * 1000L,
-                                         Integer.parseInt(getProperty("log4j2.elasticsearch.bulk.retry.count", "LOG4J2_ELASTICSEARCH_BULK_RETRY_COUNT", bulkRetryCount, "5")),
-                                         Long.parseLong(getProperty("log4j2.elasticsearch.bulk.retry.delay", "LOG4J2_ELASTICSEARCH_BULK_RETRY_DELAY", bulkRetryDelay, "5")) * 1000L,
-                                         Integer.parseInt(getProperty("log4j2.elasticsearch.event.size.start.finish", "LOG4J2_ELASTICSEARCH_EVENT_SIZE_START_FINISH", eventSizeStartFinish, "65536")),
-                                         Integer.parseInt(getProperty("log4j2.elasticsearch.event.size.default", "LOG4J2_ELASTICSEARCH_EVENT_SIZE_DEFAULT", eventSizeDefault, "1536")),
-                                         Integer.parseInt(getProperty("log4j2.elasticsearch.event.size.exception", "LOG4J2_ELASTICSEARCH_EVENT_SIZE_EXCEPTION", eventSizeException, "8192")),
-                                         Integer.parseInt(getProperty("log4j2.elasticsearch.length.string.max", "LOG4J2_ELASTICSEARCH_LENGTH_STRING_MAX", lengthStringMax, "65536")),
-                                         Boolean.parseBoolean(getProperty("log4j2.elasticsearch.out", "LOG4J2_ELASTICSEARCH_OUT", out, "true")),
-                                         Boolean.parseBoolean(getProperty("log4j2.elasticsearch.debug", "LOG4J2_ELASTICSEARCH_DEBUG", debug, "false")),
-                                         Boolean.parseBoolean(getProperty("log4j2.elasticsearch.set.default.uncaught.exception.handler", "LOG4J2_ELASTICSEARCH_SET_DEFAULT_UNCAUGHT_EXCEPTION_HANDLER", setDefaultUncaughtExceptionHandler, "true")),
-                                         filter,
-                                         layout);
+        ElasticSearchAppender esa = INSTANCE.get();
+        if (esa != null) {
+            return esa;
+        } else {
+            esa = new ElasticSearchAppender((name != null) ? name : "elasticsearch",
+                                            getProperty("log4j2.elasticsearch.url", "LOG4J2_ELASTICSEARCH_URL", url, null),
+                                            getProperty("log4j2.elasticsearch.user", "LOG4J2_ELASTICSEARCH_USER", user, null),
+                                            getProperty("log4j2.elasticsearch.password", "LOG4J2_ELASTICSEARCH_PASSWORD", password, null),
+                                            getProperty("log4j2.elasticsearch.index", "LOG4J2_ELASTICSEARCH_INDEX", index, null),
+                                            Boolean.parseBoolean(getProperty("log4j2.elasticsearch.enable", "LOG4J2_ELASTICSEARCH_ENABLE", enable, "true")),
+                                            Integer.parseInt(getProperty("log4j2.elasticsearch.count.max", "LOG4J2_ELASTICSEARCH_COUNT_MAX", countMax, "20000")),
+                                            Long.parseLong(getProperty("log4j2.elasticsearch.size.max", "LOG4J2_ELASTICSEARCH_SIZE_MAX", sizeMax, "10485760")),
+                                            Integer.parseInt(getProperty("log4j2.elasticsearch.bulk.count.max", "LOG4J2_ELASTICSEARCH_BULK_COUNT_MAX", bulkCountMax, "8000")),
+                                            Long.parseLong(getProperty("log4j2.elasticsearch.bulk.size.max", "LOG4J2_ELASTICSEARCH_BULK_SIZE_MAX", bulkSizeMax, "4194304")),
+                                            Long.parseLong(getProperty("log4j2.elasticsearch.delay.max", "LOG4J2_ELASTICSEARCH_DELAY_MAX", delayMax, "60")) * 1000L,
+                                            Integer.parseInt(getProperty("log4j2.elasticsearch.bulk.retry.count", "LOG4J2_ELASTICSEARCH_BULK_RETRY_COUNT", bulkRetryCount, "5")),
+                                            Long.parseLong(getProperty("log4j2.elasticsearch.bulk.retry.delay", "LOG4J2_ELASTICSEARCH_BULK_RETRY_DELAY", bulkRetryDelay, "5")) * 1000L,
+                                            Integer.parseInt(getProperty("log4j2.elasticsearch.event.size.start.finish", "LOG4J2_ELASTICSEARCH_EVENT_SIZE_START_FINISH", eventSizeStartFinish, "65536")),
+                                            Integer.parseInt(getProperty("log4j2.elasticsearch.event.size.default", "LOG4J2_ELASTICSEARCH_EVENT_SIZE_DEFAULT", eventSizeDefault, "1536")),
+                                            Integer.parseInt(getProperty("log4j2.elasticsearch.event.size.exception", "LOG4J2_ELASTICSEARCH_EVENT_SIZE_EXCEPTION", eventSizeException, "8192")),
+                                            Integer.parseInt(getProperty("log4j2.elasticsearch.length.string.max", "LOG4J2_ELASTICSEARCH_LENGTH_STRING_MAX", lengthStringMax, "65536")),
+                                            Boolean.parseBoolean(getProperty("log4j2.elasticsearch.out", "LOG4J2_ELASTICSEARCH_OUT", out, "true")),
+                                            Boolean.parseBoolean(getProperty("log4j2.elasticsearch.debug", "LOG4J2_ELASTICSEARCH_DEBUG", debug, "false")),
+                                            Boolean.parseBoolean(getProperty("log4j2.elasticsearch.set.default.uncaught.exception.handler", "LOG4J2_ELASTICSEARCH_SET_DEFAULT_UNCAUGHT_EXCEPTION_HANDLER", setDefaultUncaughtExceptionHandler, "true")),
+                                            filter,
+                                            layout);
+            if (INSTANCE.compareAndSet(null, esa)) {
+                return esa;
+            } else {
+                return INSTANCE.get();
+            }
+        }
     }
 
     public static void logSystem(boolean out, Class clazz, String message) {
